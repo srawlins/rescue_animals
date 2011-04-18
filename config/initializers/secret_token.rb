ab2648526f417ca724bdd375d0ea377d9f3084d3
srawlins <sam.rawlins@gmail.com>
2011-04-18 16:11:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RescueAnimals::Application.config.secret_token = 'a98c6473ab68413349704e73b1e4770fd6c84349ea11dffc78818c811b80189065ef86e86ee096e240113c5a3f76045db427c06bf023c786209c1ec2475cb9af'
